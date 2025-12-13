06911c0bcbc27e1a1ce9c9823d848d9b983cc717
Antonio Glavocevic <antonio.gl1991@gmail.com>
2025-12-13 21:22:36 +1100

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -14,5 +15,9 @@ export default defineNuxtConfig({
         },
       ],
     },
+  },
+  css: ["./app/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
